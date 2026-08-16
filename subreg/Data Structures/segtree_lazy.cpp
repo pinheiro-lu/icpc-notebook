b62c1130e9464b8b7d7004c1149b88a9c92174df
pinheiro-lu <luanpereira0603@gmail.com>
2026-08-16 15:29:25 -0300

    update segtree lazy to KACTL implementation

--- subreg/Data Structures/segtree_lazy.cpp
+++ subreg/Data Structures/segtree_lazy.cpp
@@ -1,50 +1,62 @@
-// SegTree
-//
-// Recursiva com Lazy Propagation
-// Query: soma do range [a, b]
-// Update: soma x em cada elemento do range [a, b]
-//
-// Complexidades:
-// build - O(n)
-// query - O(log(n))
-// update - O(log(n))
+// Lazy Segment Tree (KACTL)
+// Description: Segment tree with ability to add or set values of large intervals,
+// and compute max of intervals. Dynamically creates nodes (implicit sparse).
+// Time: O(log N)
+// Usage: Node* tr = new Node(v, 0, v.size()); // from vi
+//        Node* tr = new Node(0, N);           // empty interval [0, N)
+// Source: https://github.com/kth-competitive-programming/kactl/blob/main/content/data-structures/LazySegmentTree.h
 
-namespace seg {
-	ll seg[4*N], lazy[4*N];
-	int n, *v;
+const int inf = 1e9;
 
-	ll build(int p=1, int l=0, int r=n-1) {
-		lazy[p] = 0;
-		if (l == r) return seg[p] = v[l];
-		int m = (l+r)/2;
-		return seg[p] = build(2*p, l, m) + build(2*p+1, m+1, r);
+struct Node {
+	Node *l = 0, *r = 0;
+	int lo, hi, mset = inf, madd = 0, val = -inf;
+
+	Node(int lo, int hi) : lo(lo), hi(hi) {}
+	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
+		if (lo + 1 < hi) {
+			int mid = lo + (hi - lo) / 2;
+			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
+			val = max(l->val, r->val);
+		} else val = v[lo];
 	}
-	void build(int n2, int* v2) {
-		n = n2, v = v2;
-		build();
+
+	int query(int L, int R) {
+		if (R <= lo || hi <= L) return -inf;
+		if (L <= lo && hi <= R) return val;
+		push();
+		return max(l->query(L, R), r->query(L, R));
 	}
-	void prop(int p, int l, int r) {
-		seg[p] += lazy[p]*(r-l+1);
-		if (l != r) lazy[2*p] += lazy[p], lazy[2*p+1] += lazy[p];
-		lazy[p] = 0;
-	}
-	ll query(int a, int b, int p=1, int l=0, int r=n-1) {
-		prop(p, l, r);
-		if (a <= l and r <= b) return seg[p];
-		if (b < l or r < a) return 0;
-		int m = (l+r)/2;
-		return query(a, b, 2*p, l, m) + query(a, b, 2*p+1, m+1, r);
-	}
-	ll update(int a, int b, int x, int p=1, int l=0, int r=n-1) {
-		prop(p, l, r);
-		if (a <= l and r <= b) {
-			lazy[p] += x;
-			prop(p, l, r);
-			return seg[p];
+
+	void set(int L, int R, int x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) mset = val = x, madd = 0;
+		else {
+			push(), l->set(L, R, x), r->set(L, R, x);
+			val = max(l->val, r->val);
 		}
-		if (b < l or r < a) return seg[p];
-		int m = (l+r)/2;
-		return seg[p] = update(a, b, x, 2*p, l, m) +
-			update(a, b, x, 2*p+1, m+1, r);
+	}
+
+	void add(int L, int R, int x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) {
+			if (mset != inf) mset += x;
+			else madd += x;
+			val += x;
+		} else {
+			push(), l->add(L, R, x), r->add(L, R, x);
+			val = max(l->val, r->val);
+		}
+	}
+
+	void push() {
+		if (!l) {
+			int mid = lo + (hi - lo) / 2;
+			l = new Node(lo, mid); r = new Node(mid, hi);
+		}
+		if (mset != inf)
+			l->set(lo, hi, mset), r->set(lo, hi, mset), mset = inf;
+		else if (madd)
+			l->add(lo, hi, madd), r->add(lo, hi, madd), madd = 0;
 	}
 };
