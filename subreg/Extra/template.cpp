7fc8bdd65aa05388e64460ffbdd6f73779e28ba9
pinheiro-lu <luanpereira0603@gmail.com>
2026-08-23 21:04:54 -0300

    add bst, update template and integer geometry with complex

--- subreg/Extra/template.cpp
+++ subreg/Extra/template.cpp
@@ -1,22 +1,17 @@
 // Template
 #include <bits/stdc++.h>
+
 using namespace std;
 
-using ll = long long;
+typedef long long ll;
+typedef vector<int> vi;
 
-#define all(x) x.begin(), x.end()
-#define int ll
+#define f first
+#define s second
 #define pb push_back
 
-void dbg_out() { cerr << endl; }
-template <typename H, typename... T>
-void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
-#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+signed main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
 
-void solve() {
-}
-
-signed main(){
-	ios::sync_with_stdio(false); cin.tie(0);
-	solve();
+	return 0;
 }
--- subreg/Geometry/integer.cpp
+++ subreg/Geometry/integer.cpp
@@ -1,90 +1,83 @@
-// Integer Geometry
-#define int long long
+// Integer Geometry with std::complex
+// Sources:
+// - Antti Laaksonen (Competitive Programmer's Handbook, Ch. 29)
+// - Victor Lecomte (Handbook of Geometry for Competitive Programmers)
+// - Codeforces Blog 22175 (https://codeforces.com/blog/entry/22175)
 
-bool zero(int x) {
-	return x == 0;
+using T = ll;
+using P = complex<T>;
+#define X real()
+#define Y imag()
+
+namespace std {
+	bool operator<(P a, P b) {
+		return a.X != b.X ? a.X < b.X : a.Y < b.Y;
+	}
 }
 
-// CORNER: point = (0, 0)
-struct point {
-	int x, y;
-	
-	point(): x(0), y(0) {}
-	point(int _x, int _y): x(_x), y(_y) {}
-	
-	point operator+(point rhs) { return point(x+rhs.x, y+rhs.y); }
-	point operator-(point rhs) { return point(x-rhs.x, y-rhs.y); }
-	int operator*(point rhs) { return x*rhs.x + y*rhs.y; }
-	int operator^(point rhs) { return x*rhs.y - y*rhs.x; }
+// Dot & Cross products: conj(a) * b = (a . b) + i * (a x b)
+T dot(P a, P b) { return (conj(a) * b).X; }
+T cross(P a, P b) { return (conj(a) * b).Y; }
 
-	int norm2() { return *this * *this; }
-
-	using tup = tuple<int, int>;
-
-	bool operator<(const point& rhs) const {
-		return tup{x, y} < tup{rhs.x, rhs.y};
-	}
-	
-	bool operator==(const point& rhs) const {
-		return tup{x, y} == tup{rhs.x, rhs.y};
-	}
-};
+bool zero(T x) {
+	return x == 0;
+}
 
 // angular comparison in [0, 2pi)
 // smallest is (1, 0)
 // CORNER: a || b == (0, 0)
-bool ang_cmp(point a, point b) {
-	auto quad = [](point p) -> bool {
+bool ang_cmp(P a, P b) {
+	auto quad = [](P p) -> bool {
 		// 0 if ang in [0, pi), 1 if in [pi, 2pi)
-		return p.y < 0 || (p.y == 0 && p.x < 0);
+		return p.Y < 0 || (p.Y == 0 && p.X < 0);
 	};
-	using tup = tuple<bool, int>;
-	return tup{quad(a), 0} < tup{quad(b), a^b};
+	using tup = tuple<bool, T>;
+	return tup{quad(a), 0} < tup{quad(b), cross(a, b)};
 }
 
-int dist2(point p, point q) { // squared distance
-    return (p - q)*(p - q);
+T dist2(P p, P q) { // squared distance
+    return norm(p - q);
 }
 
-int area2(point a, point b, point c) { // two times signed area of triangle abc
-	return (b - a) ^ (c - a);
+T area2(P a, P b, P c) { // two times signed area of triangle abc
+	return cross(b - a, c - a);
 }
 
-bool left(point a, point b, point c) {
+bool left(P a, P b, P c) {
 	return area2(a, b, c) > 0; // counterclockwise
 }
 
-bool right(point a, point b, point c) {
+bool right(P a, P b, P c) {
 	return area2(a, b, c) < 0; // clockwise
 }
 
-bool collinear(point a, point b, point c) {
-	return zero(area2(a,b,c));
+bool collinear(P a, P b, P c) {
+	return zero(area2(a, b, c));
 }
 
 // CORNER: a || b == (0, 0)
-int parallel(point a, point b) {
-	if(!zero(a ^ b)) return 0;
-	return (a.x>0) == (b.x>0) && (a.y > 0) == (b.y > 0) ? 1 : -1;
+int parallel(P a, P b) {
+	if(!zero(cross(a, b))) return 0;
+	return (a.X > 0) == (b.X > 0) && (a.Y > 0) == (b.Y > 0) ? 1 : -1;
 }
 
 // CORNER: a == b
 struct segment {
-	point a, b;
+	P a, b;
 	
 	segment(): a(), b() {}
-	segment(point _a, point _b): a(_a), b(_b) {}
+	segment(P _a, P _b): a(_a), b(_b) {}
 
-	point vec() { return b - a; }
+	P vec() { return b - a; }
 
-	bool contains(point p) {
-		return a == p || b == p || parallel(a-p, b-p) == -1;
+	bool contains(P p) {
+		return a == p || b == p || parallel(a - p, b - p) == -1;
 	}
 };
 
 bool intersects(segment r, segment s) {
-	if(contains(r, s.a) || contains(r, s.b) || contains(s, r.a) || contains(s, r.b)) return 1;
-	return left(r.a,r.b,s.a) != left(r.a,r.b,s.b) && 
+	if(r.contains(s.a) || r.contains(s.b) || s.contains(r.a) || s.contains(r.b)) return 1;
+	return left(r.a, r.b, s.a) != left(r.a, r.b, s.b) && 
 		left(s.a, s.b, r.a) != left(s.a, s.b, r.b);
 }
 
@@ -93,41 +86,40 @@ bool parallel(segment r, segment s) {
 }
 
 struct polygon {
-	vector<point> vp;
+	vector<P> vp;
 	int n;
 
-	polygon(vector<point>& _vp): vp(_vp), n(vp.size()) {
+	polygon(vector<P>& _vp): vp(_vp), n(vp.size()) {
 		if(area2() < 0) reverse(all(_vp));
 	}
 
-	int nxt(int i) { return i+1<n ? i+1 : 0; }
-	int prv(int i) { return i ? i-1 : n-1; }
+	int nxt(int i) const { return i + 1 < n ? i + 1 : 0; }
+	int prv(int i) const { return i ? i - 1 : n - 1; }
 
 	// If positive, the polygon is in ccw order. It is in cw order otherwise.
-	int area2() { // O(n
-		int acum = 0;
+	T area2() { // O(n)
+		T acum = 0;
 		for(int i = 0; i < n; i++)
-			acum += vp[i] ^ vp[nxt(i)];
+			acum += cross(vp[i], vp[nxt(i)]);
 		return acum;
 	}
 
-	bool has(point p) { // O(log n). The polygon must be convex and in ccw order
-		if(right(vp[0], vp[1], p) || left(vp[0], vp[n-1], p)) return 0;
+	bool has(P p) { // O(log n). The polygon must be convex and in ccw order
+		if(right(vp[0], vp[1], p) || left(vp[0], vp[n - 1], p)) return 0;
 		int lo = 1, hi = n;
 		while(lo + 1 < hi) {
 			int mid = (lo + hi) / 2;
 			if(!right(vp[0], vp[mid], p)) lo = mid;
 			else hi = mid;
 		}
-		return hi != n ? !right(vp[lo], vp[hi], p) : dist2(vp[0], p) <= dist2(vp[0], vp[n-1]);
+		return hi != n ? !right(vp[lo], vp[hi], p) : dist2(vp[0], p) <= dist2(vp[0], vp[n - 1]);
 	}
 
-	int calipers() { // O(n). The polygon must be convex and in ccw order.
-		int ans = 0;
+	T calipers() { // O(n). The polygon must be convex and in ccw order.
+		T ans = 0;
 		for(int i = 0, j = 1; i < n; i++) {
-			point v = vp[nxt(i)] - vp[i];
-			while((v ^ (vp[nxt(j)] - vp[j])) > 0) j = nxt(j);
-			// do something with vp[i] and vp[j]
+			P v = vp[nxt(i)] - vp[i];
+			while(cross(v, vp[nxt(j)] - vp[j]) > 0) j = nxt(j);
 			ans = max(ans, dist2(vp[i], vp[j])); // Example with polygon diameter squared
 		}
 		return ans;
@@ -135,9 +127,9 @@ struct polygon {
 
 	// returns the maximal point using comparator cmp
 	// example: 
-	// 	extreme([&](point p, point q) {return p * v > q * v;});
+	// 	extreme([&](P p, P q) {return dot(p, v) > dot(q, v);});
 	// 	returns point with maximal dot product with v
-	int extreme(const function<bool(point, point)> &cmp) {
+	int extreme(const function<bool(P, P)> &cmp) {
 		auto is_extreme = [&](int i, bool& cur_dir) -> bool {
 			cur_dir = cmp(vp[nxt(i)], vp[i]);
 			return !cmp(vp[prv(i)], vp[i]) && !cur_dir;
@@ -157,12 +149,12 @@ struct polygon {
 		return lo;
 	}
 
-	pair<int, int> tangent(point p) { // O(log n) for convex polygon in ccw orientation
+	pair<int, int> tangent(P p) { // O(log n) for convex polygon in ccw orientation
 		// Finds the indices of the two tangents to an external point q
-		auto left_tangent = [&](point r, point s) -> bool {
+		auto left_tangent = [&](P r, P s) -> bool {
 			return right(p, r, s);
 		};
-		auto right_tangent = [&](point r, point s) -> bool {
+		auto right_tangent = [&](P r, P s) -> bool {
 			return left(p, r, s);
 		};
 		return {extreme(left_tangent), extreme(right_tangent)};
@@ -175,11 +167,10 @@ struct polygon {
 	polygon operator+(polygon& rhs) { // Minkowsky sum
 		normalize();
 		rhs.normalize();
-		vector<point> sum;
+		vector<P> sum;
 		for(int i = 0, j = 0, dir; i < n || j < rhs.n; i += dir >= 0, j += dir <= 0) {
 			sum.push_back(vp[i % n] + rhs.vp[j % rhs.n]);
-			dir = (vp[(i + 1) % n] - vp[i % n]) 
-				^ (rhs.vp[(j + 1) % rhs.n] - rhs.vp[j % rhs.n]);
+			dir = cross(vp[(i + 1) % n] - vp[i % n], rhs.vp[(j + 1) % rhs.n] - rhs.vp[j % rhs.n]);
 		}
 		return polygon(sum);
 	}
