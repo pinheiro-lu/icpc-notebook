743380ce4fec0f058e5bab45e38e46e841fe5720
pinheiro-lu <luanpereira0603@gmail.com>
2026-08-23 23:15:24 -0300

    add kruskal and dsu from Bruno Maletta (UFMG)

---
+++ subreg/Data Structures/dsu.cpp
@@ -0,0 +1,20 @@
+// DSU
+//
+// Une dois conjuntos e acha a qual conjunto um elemento pertence por seu id
+//
+// find e unite: O(a(n)) ~= O(1) amortizado
+
+struct dsu {
+	vector<int> id, sz;
+
+	dsu(int n) : id(n), sz(n, 1) { iota(id.begin(), id.end(), 0); }
+
+	int find(int a) { return a == id[a] ? a : id[a] = find(id[a]); }
+
+	void unite(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return;
+		if (sz[a] < sz[b]) swap(a, b);
+		sz[a] += sz[b], id[b] = a;
+	}
+};
